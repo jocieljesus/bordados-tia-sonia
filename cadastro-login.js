984cb41a535c285b04d4ac08385358e16712b475
jociel <jocielalvesdejesus@gmail.com>
2026-05-07 01:01:15 -0300

    feature: corrige cadastro login

---
+++ cadastro-login.js
@@ -0,0 +1,56 @@
+// --- PARTE DO CADASTRO ---
+const formCadastrar = document.getElementById("form-cadastrar");
+
+// Só executa se estivermos na página que tem o form-cadastrar
+if (formCadastrar) {
+    formCadastrar.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const usuario = {
+            nome: document.getElementById("nome").value,
+            sobrenome: document.getElementById("sobrenome").value,
+            email: document.getElementById("email").value,
+            senha: document.getElementById("senha").value,
+            ddd: document.getElementById("ddd").value,
+            telefone: document.getElementById("telefone").value,
+            endereco: document.getElementById("endereco").value,
+            numero: document.getElementById("numero").value,
+            cidade: document.getElementById("cidade").value,
+            cep: document.getElementById("cep").value,
+            dataNascimento: document.getElementById("data-nascimento").value,
+            genero: document.getElementById("genero").value
+        };
+
+        localStorage.setItem("usuarioCadastrado", JSON.stringify(usuario));
+        alert("Cadastro Realizado com Sucesso!");
+        window.location.href = "login.html"; // Vai para o login após cadastrar
+    });
+}
+
+// --- PARTE DO LOGIN ---
+const formLogar = document.getElementById("form-logar");
+
+// Só executa se estivermos na página que tem o form-logar
+if (formLogar) {
+    formLogar.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const usuarioCadastrado = localStorage.getItem("usuarioCadastrado");
+
+        if (usuarioCadastrado) {
+            const usuarioLogin = JSON.parse(usuarioCadastrado);
+            
+            var emailDigitado = document.getElementById("email").value;
+            var senhaDigitada = document.getElementById("senha").value;
+
+            if (emailDigitado === usuarioLogin.email && senhaDigitada === usuarioLogin.senha) {
+                window.alert("Usuário Logado com sucesso!");
+                window.location.href = "index.html";
+            } else {
+                window.alert("Atenção: Usuário ou Senha Incorretos");
+            }
+        } else {
+            window.alert("Nenhum usuário cadastrado encontrado!");
+        }
+    });
+}
